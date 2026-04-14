29f1f02ae096c5c927207eca2ebf63074317c553
MinaAlmasi <mina@cc.au.dk>
2026-04-14 22:44:01 +0200

    fix: make header much more simple and pretty!

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import styles from './Header.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>INTERACT-LLM</h1>
+      <h1>InteractLLM</h1>
     </header>
   );
 }
