8fabdc3480008f2b36176fdd0a8fc68b06e441e1
MinaAlmasi <mina@cc.au.dk>
2026-04-16 15:27:40 +0200

    update header to lead back to landing page with Link structure in Next.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 import styles from './Header.module.css';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>InteractLLM</h1>
+      <h1>
+        <Link href="/">InteractLLM</Link>
+      </h1>
     </header>
   );
 }
