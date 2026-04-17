28c653c6360f2137117f09ee03e52be3e4aadcfa
MinaAlmasi <mina@cc.au.dk>
2026-04-17 10:54:04 +0200

    fix: update font sizes, rm unused avatar

--- src/components/AssistantMessage/AssistantMessage.js
+++ src/components/AssistantMessage/AssistantMessage.js
@@ -3,15 +3,10 @@ import styles from "./AssistantMessage.module.css";
 
 
 export default function AssistantMessage({ content }) {
-  const avatar = (
-    <div className={styles.avatar}>AI</div>
-  );
-
   return (
     <ChatMessage
       content={content}
       bubbleClassName={styles.bubble}
-      avatar={avatar}
     />
   );
 }
