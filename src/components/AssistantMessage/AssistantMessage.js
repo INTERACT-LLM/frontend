1618595cefdd0c34ea53d0f64802b72f71ad475f
MinaAlmasi <mina@cc.au.dk>
2026-04-20 21:39:21 +0200

    display feedback, keep console log for now

--- src/components/AssistantMessage/AssistantMessage.js
+++ src/components/AssistantMessage/AssistantMessage.js
@@ -4,12 +4,10 @@ import ImmediateFeedback from '@/components/ImmediateFeedback/ImmediateFeedback'
 import styles from './AssistantMessage.module.css';
 
 export default function AssistantMessage({ content, feedback }) {
-  const hasFeedback = feedback?.has_error
   console.log('AssistantMessage feedback:', feedback);
-
   return (
     <ChatMessage content={content} className={styles.message}>
-      {hasFeedback && <ImmediateFeedback feedback={feedback} />}
+      {<ImmediateFeedback feedback={feedback} />}
     </ChatMessage>
   );
 }
