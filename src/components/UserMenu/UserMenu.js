d902073e21ad33abdaeef81ad03063c098ef5db9
MinaAlmasi <mina@cc.au.dk>
2026-04-24 12:00:20 +0200

    rename from user provider to usermenu (more accurate) and import usercontext

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,9 +1,10 @@
+// has user avatar and modal for editing user preferences: is what sits in header
 "use client";
 import UserAvatar from "@/components/UserAvatar/UserAvatar";
 import UserModal from "@/components/UserModal/UserModal";
-import useUser from "@/hooks/UseUser";
+import { useUser } from "@/context/UserContext";
 
-export default function UserProvider() {
+export default function UserMenu() {
   const { user, showModal, openModal, modalProps } = useUser();
   return (
     <>
