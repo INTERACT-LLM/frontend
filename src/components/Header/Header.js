5337477af6507baf00a7d003274b1d686cd02bdf
MinaAlmasi <mina@cc.au.dk>
2026-04-27 23:01:15 +0200

    hide model status banner from header on landing page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,21 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import styles from './Header.module.css';
 import UserMenu from '@/components/UserMenu/UserMenu';
 import ModelStatusBanner from '@/components/ModelStatusBanner/ModelStatusBanner';
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header className={styles.header}>
       <h1>
         <Link href="/">InteractLLM</Link>
       </h1>
       <div className={styles.controls}>
-        <ModelStatusBanner />
+        {pathname !== '/' && <ModelStatusBanner />}
         <UserMenu />
       </div>
     </header>
