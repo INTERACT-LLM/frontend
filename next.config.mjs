d623b5d440e3faba90129e09a2d94c042f57de74
MinaAlmasi <mina@cc.au.dk>
2026-04-30 11:12:27 +0200

    feat: add environment variable configuration for API_BASE_URL in nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL || "http://127.0.0.1:8000",
+  },
 };
 
 export default nextConfig;
